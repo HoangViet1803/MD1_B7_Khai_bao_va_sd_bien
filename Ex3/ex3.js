43ab868b958942dbb49ac43a9d76c9ea1b2ab80b
HoangViet1803 <viethenry1803@gmail.com>
2022-10-10 14:50:18 +0700

    add ex

---
+++ Ex3/ex3.js
@@ -0,0 +1,7 @@
+let a = prompt("Nhập vào số thứ nhất:");
+let b = prompt("Nhập vào số thứ hai:");
+if (a%b === 0) {
+    alert("Số thứ nhất là bội của số thứ hai.")
+} else {
+    alert("Số thứ nhất không phải là bội của số thứ hai")
+}
